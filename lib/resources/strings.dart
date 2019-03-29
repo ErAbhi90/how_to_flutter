2cc00195b73a25c382508c6e8653c73d82b82a82
ErAbhi90 <er.abhithakral@gmail.com>
2019-03-29 13:49:29 +0530

    Container Rectangle Decoration

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -1,4 +1,5 @@
 class Strings {
   static final textData = "This is centered Text";
   static final circleText = "This is Text Circular Container";
+  static final rectangleText = "This is Text Rectangle Container";
 }
--- lib/ui/MyApp.dart
+++ lib/ui/MyApp.dart
@@ -6,7 +6,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.grey,
-      /* Container with just color and no other decoration*/
+      /* 
+      Different Container Properties
+      
+      01 - Container with just color and no other decoration
+      
+      */
       /*
         child: Container(
         color: Colors.grey,
@@ -23,14 +28,20 @@ class MyApp extends StatelessWidget {
         ),
       ), */
 
-      /* Container with box decoration
+      /* 
+
+      02 -Container with box decoration
       Container shape - Circle
                 color - Orange 
                 border_all - width is 2.0 and color red
                 style - Solid
+
+                margin - all sides using EdgeInsets all property
+                padding - defined using symmetry of horizontal and vertical
+
        */
 
-      child: Container(
+      /*  child: Container(
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           color: Colors.orange,
@@ -48,6 +59,56 @@ class MyApp extends StatelessWidget {
         child: Center(
           child: Text(
             Strings.circleText,
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      ), */
+
+/* 
+
+      03 -Container with box decoration
+      Container shape - Rectangle
+                color - Teal 
+                border_only sides- width is 2.0 and color green[900]
+                style - Solid
+
+                margin - seprately defined using Edge Insets only
+                padding - defined using symmetry of horizontal and vertical
+                
+       */
+
+      child: Container(
+        decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.teal,
+            border: Border(
+              left: BorderSide(
+                color: Colors.green[900],
+                width: 2.0,
+              ),
+              right: BorderSide(
+                color: Colors.green[900],
+                width: 2.0,
+              ),
+            )),
+        margin: EdgeInsets.only(
+          left: 20.0,
+          right: 20.0,
+          top: 30.0,
+          bottom: 30.0,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 10.0,
+          vertical: 10.0,
+        ),
+        child: Center(
+          child: Text(
+            Strings.rectangleText,
             textAlign: TextAlign.center,
             textDirection: TextDirection.ltr,
             style: TextStyle(
