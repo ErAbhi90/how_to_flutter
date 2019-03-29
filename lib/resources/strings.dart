d83f6f0ecff17cff043f6cc0311aca8e1642deb8
ErAbhi90 <er.abhithakral@gmail.com>
2019-03-29 13:37:13 +0530

    Container with box decoration Circlular

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -1,3 +1,4 @@
 class Strings {
   static final textData = "This is centered Text";
+  static final circleText = "This is Text Circular Container";
 }
--- lib/ui/MyApp.dart
+++ lib/ui/MyApp.dart
@@ -5,11 +5,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
+      color: Colors.grey,
+      /* Container with just color and no other decoration*/
+      /*
+        child: Container(
         color: Colors.grey,
         child: Center(
           child: Text(
             Strings.textData,
+            textAlign: TextAlign.center,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0,
+            ),
+          ),
+        ),
+      ), */
+
+      /* Container with box decoration
+      Container shape - Circle
+                color - Orange 
+                border_all - width is 2.0 and color red
+                style - Solid
+       */
+
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.orange,
+          border: Border.all(
+            width: 2.0,
+            color: Colors.red,
+            style: BorderStyle.solid,
+          ),
+        ),
+        margin: EdgeInsets.all(20.0),
+        padding: EdgeInsets.symmetric(
+          horizontal: 10.0,
+          vertical: 20.0,
+        ),
+        child: Center(
+          child: Text(
+            Strings.circleText,
             textAlign: TextAlign.center,
             textDirection: TextDirection.ltr,
             style: TextStyle(
